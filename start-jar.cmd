31ca819a96f0b4cde0686192f443b77c4a34f80e
nguen <104498646+gawgua@users.noreply.github.com>
2025-10-03 00:24:47 +0700

    fix: detach console from .jar

--- start-jar.cmd
+++ start-jar.cmd
@@ -1,1 +1,3 @@
-java --module-path target/lib --add-modules javafx.controls,javafx.graphics -cp "target/arkanoid-1.0.jar;target/lib/*" -jar ./target/arkanoid-1.0.jar
+@echo off
+start "" javaw --module-path target/lib --add-modules javafx.controls,javafx.graphics,javafx.media -cp "target/arkanoid-1.0.jar;target/lib/*" -jar ./target/arkanoid-1.0.jar
+exit
