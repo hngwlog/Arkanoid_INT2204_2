a25f1ce7e5d665daeab1585c289eb1fdbd2e2abc
nguen <104498646+gawgua@users.noreply.github.com>
2025-10-03 09:56:02 +0700

    feat: audio

--- src/com/raumania/gui/screen/SettingScreen.java
+++ src/com/raumania/gui/screen/SettingScreen.java
@@ -1,5 +1,6 @@
 package com.raumania.gui.screen;
 
+import com.raumania.core.AudioManager;
 import com.raumania.gui.manager.SceneManager;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
@@ -16,21 +17,18 @@ public class SettingScreen extends Screen {
         // put components here
 
         //Volume Text
-        Text volumeText = newText("Volume: 50%", 100, 100, 2.0, 2.0);
+        Text volumeText = newText("Volume: " + AudioManager.getInstance().getVolume() + "%", 100, 100, 2.0, 2.0);
         root.getChildren().add(volumeText);
 
         //Volume Slider
-        Slider slider = newSlider(0, 50, 100, 400, 100,
+        Slider slider = newSlider(0, AudioManager.getInstance().getVolume() * 100, 100, 400, 100,
                 3.0, 3.0, false, false);
-        root.getChildren().add(slider);
-
+        slider.valueProperty().bindBidirectional(AudioManager.getInstance().getVolumeProperty());
         //Slider change value -> volume
         slider.valueProperty().addListener((obs, oldVal, newVal) -> {
-            int volume = newVal.intValue();
-            volumeText.setText("Volume: " + volume + "%");
-            // Nếu dùng MediaPlayer: mediaPlayer.setVolume(volume / 100.0);
-
+            volumeText.setText("Volume: " + newVal.intValue() + "%");
         });
+        root.getChildren().add(slider);
 
         //Back to Home button
         Button back = centerButton("Back to Home", 500, 2.0, 2.0);
@@ -38,7 +36,5 @@ public class SettingScreen extends Screen {
             sceneManager.switchScreen(ScreenType.HOME);
         });
         root.getChildren().add(back);
-
     }
-
 }
