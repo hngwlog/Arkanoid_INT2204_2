d0d581e1c557c0e90274130d66c629e6aac1e0f5
nguen <104498646+gawgua@users.noreply.github.com>
2025-10-14 14:11:58 +0700

    refactor: use addAll in all screens

--- src/com/raumania/gui/screen/HomeScreen.java
+++ src/com/raumania/gui/screen/HomeScreen.java
@@ -1,7 +1,9 @@
 package com.raumania.gui.screen;
 
 import com.raumania.core.AudioManager;
+import com.raumania.core.SpriteSheet;
 import com.raumania.gui.manager.SceneManager;
+import com.raumania.utils.ResourcesLoader;
 import javafx.application.Platform;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -15,41 +17,36 @@ public class HomeScreen extends Screen {
         super(sceneManager);
 
         //Game title
-        root.getChildren().clear();
         // put components here
         Text title = centerText("Welcome to Arkanoid!", 100, 3.0, 3.0);
         // style components
         title.setFill(Color.GREEN);
-        // add components to root
-        root.getChildren().add(title);
 
         //Play button
         Button play =  centerButton("Play", 200, 2.0, 2.0);
         play.setOnAction(e -> {
             sceneManager.switchScreen(ScreenType.GAME);
         });
-        root.getChildren().add(play);
 
         //Level select button
         Button levelSelect =  centerButton("Level Select", 300, 2.0, 2.0);
         levelSelect.setOnAction(e -> {
             sceneManager.switchScreen(ScreenType.LEVEL_SELECT);
         });
-        root.getChildren().add(levelSelect);
 
         //Setting button
         Button setting = centerButton("Setting", 400, 2.0, 2.0);
         setting.setOnAction(e -> {
             sceneManager.switchScreen(ScreenType.SETTINGS);
         });
-        root.getChildren().add(setting);
 
         //Quit button
         Button quit = centerButton("Quit", 500, 2.0, 2.0);
         quit.setOnAction(e -> {
             Platform.exit();
         });
-        root.getChildren().add(quit);
+
+        root.getChildren().addAll(title, play, levelSelect, setting, quit);
     }
 
     @Override
