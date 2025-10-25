c7f703a1b5d8b46f86d55bc4c508652ac412f611
nguen <104498646+gawgua@users.noreply.github.com>
2025-10-25 23:22:58 +0700

    fix: javafx not init in test

--- test/com/raumania/gameplay/objects/NormalBrickTest.java
+++ test/com/raumania/gameplay/objects/NormalBrickTest.java
@@ -1,10 +1,14 @@
 package com.raumania.gameplay.objects;
 
+import com.raumania.utils.InitializeJavaFx;
+import javafx.application.Platform;
+import javafx.scene.layout.Pane;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class NormalBrickTest {
+class NormalBrickTest extends InitializeJavaFx {
     @Test
     void testTakeHit() {
         NormalBrick nb = new NormalBrick(0, 0, 1, 1);
