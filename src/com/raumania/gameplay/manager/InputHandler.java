cecad8dcff59bcb959f15d9303e900dcd1367c96
nguen <104498646+gawgua@users.noreply.github.com>
2025-10-30 22:16:08 +0700

    fix: input in same thread blocking each other in multiplayer mode

---
+++ src/com/raumania/gameplay/manager/InputHandler.java
@@ -0,0 +1,90 @@
+package com.raumania.gameplay.manager;
+
+import javafx.scene.input.KeyCode;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class InputHandler {
+    // thread-safe gameState
+    private final AtomicBoolean running = new AtomicBoolean(false);
+    private Thread thread;
+    private GameManager gameManager;
+    private KeyCode leftKey;
+    private KeyCode rightKey;
+    // keep track of key up or down
+    private boolean leftPressed = false;
+    private boolean rightPressed = false;
+
+    public InputHandler(GameManager gameManager, KeyCode leftKey, KeyCode rightKey) {
+        this.gameManager = gameManager;
+        this.leftKey = leftKey;
+        this.rightKey = rightKey;
+    }
+
+    /**
+     * Spawn a new thread to handle input
+     */
+    public void start() {
+        // avoid spam thread
+        if (running.get()) {
+            return;
+        }
+
+        running.set(true);
+        thread = new Thread(() -> {
+            while (running.get()) {
+                // if not pressed, send release event will not hurt
+                if (leftPressed)
+                    gameManager.handleInput(leftKey, true);
+                else
+                    gameManager.handleInput(leftKey, false);
+
+                if (rightPressed)
+                    gameManager.handleInput(rightKey, true);
+                else
+                    gameManager.handleInput(rightKey, false);
+
+                try {
+                    // avoid full CPU
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    stop();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    /**
+     * Stop the input handler thread
+     */
+    public void stop() {
+        running.set(false);
+        if (thread != null) {
+            thread.interrupt();
+        }
+    }
+
+    /**
+     * Flag the key as pressed
+     * @param keyCode the inp key
+     */
+    public void onKeyPressed(KeyCode keyCode) {
+        if (keyCode == leftKey) {
+            leftPressed = true;
+        } else if (keyCode == rightKey) {
+            rightPressed = true;
+        }
+    }
+
+    /**
+     * Flag the key as released
+     * @param keyCode the inp key
+     */
+    public void onKeyReleased(KeyCode keyCode) {
+        if (keyCode == leftKey) {
+            leftPressed = false;
+        } else if (keyCode == rightKey) {
+            rightPressed = false;
+        }
+    }
+}
