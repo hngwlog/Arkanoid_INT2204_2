3d0eb601d00c4caa3f3013fb9a2d5ab001e71e37
tuld1806 <ldt10082006@gmail.com>
2025-11-06 19:41:40 +0700

    javadoc
    add javadoc for boss, powerups

--- src/com/raumania/gameplay/objects/powerup/AddBallPowerUp.java
+++ src/com/raumania/gameplay/objects/powerup/AddBallPowerUp.java
@@ -8,9 +8,33 @@ import com.raumania.utils.ResourcesLoader;
 import java.awt.*;
 import java.util.List;
 
+/**
+ * Represents the "Add Ball" power-up in the game.
+ * <p>
+ * When collected by the player, this power-up duplicates all existing balls
+ * up to a maximum of 8 total balls. Each new ball inherits the position
+ * and direction of its source ball at the moment of duplication.
+ * </p>
+ * <p>
+ * The visual appearance of this power-up is defined by a sprite sheet named
+ * {@code "addballpowerup.png"}.
+ * </p>
+ *
+ * @see PowerUp
+ * @see com.raumania.gameplay.objects.Ball
+ * @see com.raumania.gameplay.manager.GameManager
+ */
 public class AddBallPowerUp extends PowerUp {
     private static final int powerUpCounter = 0;
 
+    /**
+     * Constructs a new AddBallPowerUp instance with specified position and size.
+     *
+     * @param x       the X position of the power-up
+     * @param y       the Y position of the power-up
+     * @param width   the width of the power-up sprite
+     * @param height  the height of the power-up sprite
+     */
     public AddBallPowerUp(double x, double y, double width, double height) {
         super(x, y, width, height, PowerUpType.ADD_BALL);
         SpriteSheet texture = new SpriteSheet(
@@ -19,6 +43,15 @@ public class AddBallPowerUp extends PowerUp {
         setPowerUpTexture(texture);
     }
 
+    /**
+     * Applies the "Add Ball" effect to the game.
+     * <p>
+     * For each currently active ball, this method spawns a new ball at the same position
+     * and with the same direction, up to a maximum total of 8 balls.
+     * </p>
+     *
+     * @param gameManager the {@link GameManager} controlling all active game objects
+     */
     @Override
     public void applyEffect(GameManager gameManager) {
         List<Ball> balls = gameManager.getBallsList();
@@ -29,6 +62,14 @@ public class AddBallPowerUp extends PowerUp {
         }
     }
 
+    /**
+     * Returns the internal counter value of this power-up type.
+     * <p>
+     * Currently, this always returns {@code 0} since no active tracking is implemented.
+     * </p>
+     *
+     * @return the counter value (always {@code 0})
+     */
     @Override
     public int getCounter() {
         return powerUpCounter;
--- src/com/raumania/gameplay/objects/powerup/ImmortalPowerUp.java
+++ src/com/raumania/gameplay/objects/powerup/ImmortalPowerUp.java
@@ -10,9 +10,40 @@ import javafx.util.Duration;
 
 import java.util.List;
 
+/**
+ * Represents the "Immortal" power-up in the game.
+ * <p>
+ * When activated, this power-up makes all active balls in the game immune to death
+ * (for example, they will not be lost when hitting the bottom of the screen).
+ * The effect lasts for a limited time, after which the balls return to their
+ * normal, mortal state.
+ * </p>
+ * <p>
+ * If multiple "Immortal" power-ups are collected consecutively, their durations stack.
+ * The immortality effect will only end once all active timers have expired.
+ * </p>
+ * <p>
+ * The visual representation of this power-up is defined by the sprite image
+ * {@code "immortalpowerup.png"}.
+ * </p>
+ *
+ * @see PowerUp
+ * @see com.raumania.gameplay.objects.Ball
+ * @see com.raumania.gameplay.manager.GameManager
+ */
 public class ImmortalPowerUp extends PowerUp {
+
+    /** Tracks the number of currently active "Immortal" power-ups. */
     protected static int powerUpCounter = 0;
 
+    /**
+     * Constructs a new ImmortalPowerUp instance with specified position and size.
+     *
+     * @param x       the X position of the power-up
+     * @param y       the Y position of the power-up
+     * @param width   the width of the power-up sprite
+     * @param height  the height of the power-up sprite
+     */
     public ImmortalPowerUp(double x, double y, double width, double height) {
         super(x, y, width, height, PowerUpType.IMMORTAL);
         SpriteSheet texture =
@@ -20,14 +51,27 @@ public class ImmortalPowerUp extends PowerUp {
         setPowerUpTexture(texture);
     }
 
+    /**
+     * Applies the "Immortal" effect to all active balls in the game.
+     * <p>
+     * Sets all balls to immortal mode and starts a timer to revert them
+     * to their normal state after the duration expires. If multiple instances
+     * of this power-up are active, immortality persists until the last timer
+     * finishes.
+     * </p>
+     *
+     * @param gameManager the {@link GameManager} managing game objects and state
+     */
     @Override
     public void applyEffect(GameManager gameManager) {
         powerUpCounter++;
         List<Ball> balls = gameManager.getBallsList();
+        // Enable immortality for all active balls
         for (Ball ball : balls) {
             ball.setImmortal(true);
         }
 
+        // Schedule deactivation after the effect duration expires
         PauseTransition timer = new PauseTransition(Duration.seconds(getDuration()));
         timer.setOnFinished(
                 e -> {
@@ -41,6 +85,11 @@ public class ImmortalPowerUp extends PowerUp {
         timer.play();
     }
 
+    /**
+     * Returns the number of currently active "Immortal" power-up effects.
+     *
+     * @return the number of active immortality effects
+     */
     @Override
     public int getCounter() {
         return powerUpCounter;
